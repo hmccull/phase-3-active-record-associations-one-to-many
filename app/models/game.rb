03e4d6ef6566cf10c0084a0db8f4fd983744c8bf
Hannah McCullough <hmac091@gmail.com>
2022-01-18 19:26:43 -0700

    commit tree a796d616988539a7e947514390f5c598a5d55bbc

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ActiveRecord::Base
-  
+    # the symbol passed to has_many is plural
+    has_many :reviews
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,12 @@
 class Review < ActiveRecord::Base
-  
+#   # a review belongs to a game
+#   def game
+#     # self is the review instance
+#     Game.find(self.game_id)
+#   end
+
+# instead of using the above custom method, we can use a macro instead, which is equivalent
+# belongs_to is a method that is inherited from ActiveRecord::Base
+# the symbol is singular that is passed to belongs_to 
+belongs_to :game
 end
